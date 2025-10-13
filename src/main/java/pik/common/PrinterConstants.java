76722dafde080ce8156731a3b7d564d2880b6e78
Martin Šustík <sustik@herman.cz>
2025-10-13 14:30:51 +0200

    Fix(PrinterConstants): Small constant corrections.

--- src/main/java/pik/common/PrinterConstants.java
+++ src/main/java/pik/common/PrinterConstants.java
@@ -12,8 +12,8 @@ public final class PrinterConstants {
     public static final int DEFAULT_CONNECTION_TIMEOUT = 10000;
     public static final int DEFAULT_PORT = 9100;
     public static final int STATUS_CHECK_INTERVAL = 5000;
-    public static final int BRIGHTNESS_MIN = 0;
-    public static final int BRIGHTNESS_MAX = 100;
+    public static final int BRIGHTNESS_MIN = 1;
+    public static final int BRIGHTNESS_MAX = 4;
     public static final int GRAYSCALE_THRESHOLD = 128;
     public static final int FULL_CUT_PERCENTAGE = 100;
     public static final int CONNECTION_STABILIZATION_DELAY_MS = 500;
